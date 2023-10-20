72f6e030b430ce716ffc3cbec36ec1caa1108d6e
rakaiwangsasatia <rakai.wangsasatia@id.tricorglobal.com>
2023-10-20 09:34:12 +0700

    kotlin dsl configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
     id ("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -31,6 +31,11 @@ android {
             )
         }
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -49,45 +54,20 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
-    implementation("com.github.bumptech.glide:glide:4.15.0")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.7")
-
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    implementation("androidx.camera:camera-camera2:1.4.0-alpha01")
-    implementation("androidx.camera:camera-lifecycle:1.4.0-alpha01")
-    implementation("androidx.camera:camera-view:1.4.0-alpha01")
-    implementation("androidx.annotation:annotation:1.7.0")
-
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
-
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
-    implementation("androidx.room:room-paging:2.6.0-rc01")
-
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
-    kapt("androidx.room:room-compiler:2.5.2")
-
-    implementation("com.facebook.shimmer:shimmer:0.5.0")
-    implementation("com.tbuonomo:dotsindicator:4.3")
-
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    lifecycle()
+    navigation()
+    image()
+    camera()
+    shimmer()
+    swipeRefresh()
+    viewPager()
+    hilt()
+    retrofit()
+    room()
+    dataStore()
+    paging()
+    unitTest()
 }
